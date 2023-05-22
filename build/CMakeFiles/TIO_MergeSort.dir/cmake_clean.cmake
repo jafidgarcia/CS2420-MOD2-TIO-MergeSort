2d0135e2217314aa1431c14694911a131ffdaac6
Jafid Garcia <jafidgarcia@gmail.com>
2023-05-22 17:41:30 -0600

    building mergeSort sub-class

---
+++ build/CMakeFiles/TIO_MergeSort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TIO_MergeSort.dir/TwoWayMerge.cpp.o"
+  "CMakeFiles/TIO_MergeSort.dir/TwoWayMerge.cpp.o.d"
+  "TIO_MergeSort"
+  "TIO_MergeSort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TIO_MergeSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/TIO_MergeSort.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TIO_MergeSort.
+# This may be replaced when dependencies are built.
